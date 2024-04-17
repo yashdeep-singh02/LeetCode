2594d9f5c5183f2844abdbde6868181e5dadcb1a
Yashdeep Singh Bhatia <87716202+yashdeep-singh02@users.noreply.github.com>
2024-04-17 15:53:31 +0530

    Time: 10 ms (19.47%) | Memory: 7.5 MB (49.53%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i = 1;
+        int res = 0;
+        while(i<=n)
+            {
+                n -=i;
+                res++;
+                i++;
+            }
+         return res;
+    }
+};
