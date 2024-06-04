9e8370f49f11e091033d4c83ca972610f04069f4
Yashdeep Singh Bhatia <87716202+yashdeep-singh02@users.noreply.github.com>
2024-06-04 14:00:28 +0530

    Time: 598 ms (18.69%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,26 +1,7 @@
 # Write your MySQL query statement below
-WITH ProcessTimes AS (
-    SELECT
-        machine_id,
-        process_id,
-        MAX(CASE WHEN activity_type = 'end' THEN timestamp ELSE NULL END) -
-        MAX(CASE WHEN activity_type = 'start' THEN timestamp ELSE NULL END) AS process_time
-    FROM
-        Activity
-    GROUP BY
-        machine_id, process_id
-),
-MachineAverages AS (
-    SELECT
-        machine_id,
-        ROUND(AVG(process_time), 3) AS processing_time
-    FROM
-        ProcessTimes
-    GROUP BY
-        machine_id
-)
-SELECT
-    machine_id,
-    processing_time
-FROM
-    MachineAverages;
+select a1.machine_id, round(avg(a2.timestamp-a1.timestamp),3) as processing_time
+from Activity a1
+join Activity a2
+on a1.machine_id=a2.machine_id and a1.process_id=a2.process_id
+and a1.activity_type='start' and a2.activity_type='end'
+group by a1.machine_id
