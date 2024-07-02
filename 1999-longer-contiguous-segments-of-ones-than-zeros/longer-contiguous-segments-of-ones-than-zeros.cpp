fcc4fb8b5b97253c4a87fa88623e9034d415a5ac
Yashdeep Singh Bhatia <87716202+yashdeep-singh02@users.noreply.github.com>
2024-07-03 03:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (14.25%) - LeetSync

---
+++ 1999-longer-contiguous-segments-of-ones-than-zeros/longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkZeroOnes(string s) {
+        int num0 =0;
+        int num1 =0;
+        int count0 = 0;
+        int count1 = 0;
+        for(int i=0;i<s.size();i++)
+           {
+            if(s[i]=='0')
+               {
+                count1= 0;
+                count0++;
+                if(num0<count0)
+                   num0 = count0;
+               }
+            else if(s[i]=='1')
+              {
+                count0 = 0;
+                count1++;
+                if(num1<count1)
+                   num1 = count1;
+               }
+           }
+        if(num1>num0)
+            return true;
+        else
+          return false;
+    }
+};
