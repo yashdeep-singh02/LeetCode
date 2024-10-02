bc90f0ffd70e84651f647392e540477c278f4181
Yashdeep Singh Bhatia <87716202+yashdeep-singh02@users.noreply.github.com>
2024-10-03 01:29:42 +0530

    Time: 39 ms (23.07%) | Memory: 16.6 MB (15.66%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -1,14 +1,7 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        count =0
-        space=0
-        for i in range(len(s)-1,-1,-1):
-            if s[i]==" " and count==0:
-                continue
-            elif s[i]!=" " :
-                count+=1
-            else:
-                break
-        return count
-        
+        # Strip leading and trailing spaces, then split the string by spaces
+        words = s.strip().split()
 
+        return len(words[-1]) if words else 0
+        
