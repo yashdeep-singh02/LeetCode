a70171e2a78cf846c8974ed12fb5cd3aec3fa26f
Yashdeep Singh Bhatia <87716202+yashdeep-singh02@users.noreply.github.com>
2024-10-10 18:50:41 +0530

    Time: 42 ms (12.92%) | Memory: 16.6 MB (15.95%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        closeOpen={')':'(',']':'[','}':'{'}
+        for c in s:
+            if c in closeOpen:
+                if stack and stack[-1]==closeOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        if stack:
+            return False
+        else:
+            return True
+        
